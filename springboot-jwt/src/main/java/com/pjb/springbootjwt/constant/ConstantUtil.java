860ca4af9760090b86207df32543afe93b267574
hfl <690328661@qq.com>
2020-04-10 22:20:40 +0800

    优化

--- springboot-jwt/src/main/java/com/pjb/springbootjwt/constant/ConstantUtil.java
+++ springboot-jwt/src/main/java/com/pjb/springbootjwt/constant/ConstantUtil.java
@@ -2,7 +2,5 @@ package com.pjb.springbootjwt.constant;
 
 public class ConstantUtil {
 
-    public  final static  String RSAPublicKey = "publicKey";//Get the key instance
-    public  final static String RSAPrivateKey  = "privateKey";//Get the key instance
 
 }
