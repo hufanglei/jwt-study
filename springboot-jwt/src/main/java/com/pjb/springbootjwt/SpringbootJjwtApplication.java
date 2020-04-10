f97badd61a1fcfc3dbb9a435fbe36add98807365
hfl <690328661@qq.com>
2020-04-10 13:54:23 +0800

    jwt设置过期时间

--- springboot-jwt/src/main/java/com/pjb/springbootjwt/SpringbootJjwtApplication.java
+++ springboot-jwt/src/main/java/com/pjb/springbootjwt/SpringbootJjwtApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.pjb.springbootjjwt.mapper")
+@MapperScan("com.pjb.springbootjwt.mapper")
 public class SpringbootJjwtApplication {
 
     public static void main(String[] args) {
--- springboot-jwt/src/main/java/com/pjb/springbootjwt/service/TokenService.java
+++ springboot-jwt/src/main/java/com/pjb/springbootjwt/service/TokenService.java
@@ -5,6 +5,9 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.pjb.springbootjwt.entity.User;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 
 /**
  * @author jinbin
@@ -14,8 +17,15 @@ import org.springframework.stereotype.Service;
 public class TokenService {
     public String getToken(User user) {
         String token="";
-        token= JWT.create().withAudience(user.getId())// 将 user id 保存到 token 里面
+        token= JWT.create()
+                .withIssuedAt(new Date(System.currentTimeMillis()))
+//                .withExpiresAt(new Date(System.currentTimeMillis()))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30*1000)) //设置过期时间).
+                .withAudience(user.getId())// 将 user id 保存到 token 里面
                 .sign(Algorithm.HMAC256(user.getPassword()));// 以 password 作为 token 的密钥
         return token;
     }
+
+
+
 }
